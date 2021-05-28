984d9785da42235a70fc6ca2cf68af8211791357
Lee Cooper <cooperle@gmail.com>
2021-05-28 13:12:38 -0500

    Init for roialign subpackage

---
+++ histomics_detect/roialign/__init__.py
@@ -0,0 +1,13 @@
+"""
+This package contains functions for region of interest alignment
+"""
+
+# make functions available at the package level using shadow imports
+from .roialign import roialign
+
+# list out things that are available for public use
+__all__ = (
+
+    # functions and classes of this package
+    'roialign'
+)
