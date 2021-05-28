8f088c73f07de31010bcbe5f0d51d0bcac08e899
Lee Cooper <cooperle@gmail.com>
2021-05-28 11:13:15 -0500

    Create __init__.py
    Init for visualization subpackage

---
+++ histomics_detect/visualization/__init__.py
@@ -0,0 +1,13 @@
+"""
+This package contains functions for visualization of detection results
+"""
+
+# make functions available at the package level using shadow imports
+from .visualization import plot_inference
+
+# list out things that are available for public use
+__all__ = (
+
+    # functions and classes of this package
+    'plot_inference'
+)
