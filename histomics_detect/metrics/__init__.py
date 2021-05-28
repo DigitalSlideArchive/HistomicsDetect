0862d531701af28fa85cd34bd31f992f98748834
Lee Cooper <cooperle@gmail.com>
2021-05-28 09:28:57 -0500

    Update __init__.py
    Handle cross module dependencies and expose iou function

--- histomics_detect/metrics/__init__.py
+++ histomics_detect/metrics/__init__.py
@@ -3,15 +3,17 @@ This package contains performance metrics implemented in tensorflow
 """
 
 # make functions available at the package level using shadow imports
-from .pr import greedy_pr_auc
+from .iou import iou
+from .iou import greedy_iou
 from .pr import greedy_pr
-from .pr import greedy_iou
+from .pr import greedy_pr_auc
 
 # list out things that are available for public use
 __all__ = (
 
     # functions and classes of this package
-    'greedy_pr_auc',
+    'iou',
+    'greedy_iou',
     'greedy_pr',
-    'greedy_iou'
+    'greedy_pr_auc'
 )
