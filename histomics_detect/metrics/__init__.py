4999a9f013d1799e1c7daaf3294821d68af3825d
Matthias Hamacher <hamachermatthias12@gmail.com>
2021-06-29 15:33:53 +0200

    implemented lmns metrics

--- histomics_detect/metrics/__init__.py
+++ histomics_detect/metrics/__init__.py
@@ -7,6 +7,7 @@ from .iou import iou
 from .iou import greedy_iou
 from .pr import greedy_pr
 from .pr import greedy_pr_auc
+from .lmns import calculate_performance_stats_lmns
 
 # list out things that are available for public use
 __all__ = (
@@ -15,5 +16,6 @@ __all__ = (
     'iou',
     'greedy_iou',
     'greedy_pr',
-    'greedy_pr_auc'
+    'greedy_pr_auc',
+    'calculate_performance_stats_lmns'
 )
