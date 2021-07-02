b242822efb79604ac8e032c7eec9901e09025ca3
Lee Cooper <cooperle@gmail.com>
2021-07-01 19:00:00 -0500

    Update augmentation __init__.py

--- histomics_detect/augmentation/__init__.py
+++ histomics_detect/augmentation/__init__.py
@@ -3,15 +3,19 @@ This package contains functions for data augmentation
 """
 
 # make functions available at the package level using shadow imports
-from .augmentation import flip
 from .augmentation import crop
+from .augmentation import flip
+from .augmentation import jitter
+from .augmentation import shrink
 from .augmentation import box_crop
 
 # list out things that are available for public use
 __all__ = (
 
     # functions and classes of this package
-    'flip',
     'crop',
+    'flip',
+    'jitter',
+    'shrink',
     'box_crop'
 )
