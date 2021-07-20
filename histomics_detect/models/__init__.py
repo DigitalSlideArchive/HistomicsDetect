eeeaaa206c07713f7da16c0a046c7a0868939c4f
Lee Cooper <cooperle@gmail.com>
2021-07-20 10:29:40 -0500

    Remove CustomCallback from FasterRCNN

--- histomics_detect/models/__init__.py
+++ histomics_detect/models/__init__.py
@@ -4,12 +4,10 @@ This package contains Keras Model subclasses for detection
 
 # make functions available at the package level using shadow imports
 from .faster_rcnn import FasterRCNN
-from .faster_rcnn import CustomCallback
 
 # list out things that are available for public use
 __all__ = (
 
     # functions and classes of this package
     'FasterRCNN',
-    'CustomCallback'
 )
