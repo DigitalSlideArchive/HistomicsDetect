2cef884205af22fb331b2679f8eff7a07ec7605f
Matthias Hamacher <hamachermatthias12@gmail.com>
2021-09-13 20:50:39 +0200

    implemented visualization for lnms

--- histomics_detect/visualization/lnms_visualization.py
+++ histomics_detect/visualization/lnms_visualization.py
@@ -7,12 +7,15 @@ import tensorflow as tf
 
 from histomics_detect.visualization.visualization import _plot_boxes
 from histomics_detect.boxes.transforms import filter_edge_boxes
+from histomics_detect.metrics import greedy_iou
+from histomics_detect.metrics.iou import iou
 
 
 def plot_inference(rgb: tf.Tensor, boxes: tf.Tensor, nms_output: tf.Tensor, rpn_boxes: tf.Tensor,
                    save_fig: bool = False, fig_path: str = 'plot_of_inference.png', filter_edge: bool = False,
                    filter_margin: int = 32, threshold: float = 0.5, figsize: Tuple[int, int] = (20, 20),
-                   gt_color: str = 'orange', pred_color: str = 'r', pred_size: int = 300):
+                   gt_colors: Tuple[str, str] = ('g', 'r'), pred_colors: Tuple[str, str] = ('g', 'r'),
+                   pred_size: int = 300, print_prediction_numbers: bool = True):
     """
     plots the already run inference
 
@@ -20,35 +23,47 @@ def plot_inference(rgb: tf.Tensor, boxes: tf.Tensor, nms_output: tf.Tensor, rpn_
     -------
 
     """
-
-    fig = plt.figure(figsize=figsize)
-    plt.imshow(rgb)
-
     scores = tf.reshape(nms_output, -1)
     condition = tf.greater(scores, threshold)
-    filtered_predictions = tf.squeeze(tf.gather(rpn_boxes, tf.where(condition)))
+    rpn_boxes = tf.squeeze(tf.gather(rpn_boxes, tf.where(condition)))
+
+    ious, _ = iou(rpn_boxes, boxes)
+
+    precision, recall, tp, fp, fn, tp_list, fp_list, fn_list = greedy_iou(ious, 0.18)
+
+    positive_pred = tf.reshape(tf.gather(rpn_boxes, tp_list[:, 0]), (-1, 4))
+    negative_pred = tf.reshape(tf.gather(rpn_boxes, fp_list), (-1, 4))  # tf.where(labels == 0)
+
+    negative_boxes = tf.reshape(tf.gather(boxes, fn_list), (-1, 4))
 
     if filter_edge:
-        width, height, _ = tf.shape(rgb)
-        filtered_boxes = filter_edge_boxes(boxes, width, height, filter_margin)
+        boxes = filter_edge_boxes(boxes, tf.shape(rgb)[1], tf.shape(rgb)[0], filter_margin)
+        negative_boxes = filter_edge_boxes(negative_boxes, tf.shape(rgb)[1], tf.shape(rgb)[0], filter_margin)
 
-        edge_filtered_predictions = filter_edge_boxes(filtered_predictions, width, height, filter_margin)
-    else:
-        filtered_boxes = boxes
+        positive_pred = filter_edge_boxes(positive_pred, tf.shape(rgb)[1], tf.shape(rgb)[0], filter_margin)
+        negative_pred = filter_edge_boxes(negative_pred, tf.shape(rgb)[1], tf.shape(rgb)[0], filter_margin)
 
-        edge_filtered_predictions = filtered_predictions
+    if print_prediction_numbers:
+        print(f'tp: {tf.shape(positive_pred)[0]}, fp: {tf.shape(negative_pred)[0]}, fn: '
+              f'{tf.shape(boxes)[0] - tf.shape(positive_pred)[0]}')
 
-    _plot_boxes(filtered_boxes, gt_color)
+    fig = plt.figure(figsize=figsize)
+    plt.imshow(tf.cast(rgb, tf.int32))
 
-    if tf.size(edge_filtered_predictions) > 0:
-        filtered_prediction_dots = edge_filtered_predictions[:, :2] + edge_filtered_predictions[:, 2:]/2
-        plt.scatter(filtered_prediction_dots[:, 0], filtered_prediction_dots[:, 1], color=pred_color, s=300)
+    _plot_boxes(boxes, gt_colors[0])  # '#b9ff38'
+    _plot_boxes(negative_boxes, gt_colors[1])  # '#b9ff38'
+
+    positive_pred_d = positive_pred[:, :2] + positive_pred[:, 2:] / 2
+    negative_pred_d = negative_pred[:, :2] + negative_pred[:, 2:] / 2
+
+    if tf.size(negative_pred_d) > 0:
+        plt.scatter(negative_pred_d[:, 0], negative_pred_d[:, 1], color=pred_colors[0], s=pred_size)
+    if tf.size(positive_pred_d) > 0:
+        plt.scatter(positive_pred_d[:, 0], positive_pred_d[:, 1], color=pred_colors[1], s=pred_size)
 
     plt.show()
     if save_fig:
         fig.savefig(fig_path)
-
-    # TODO calculate statistics for image
 
 
 def run_plot():
