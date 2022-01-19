fdc3cb7864b116b48ed6ca5b91c6cc21158b74d7
Lee Cooper <cooperle@gmail.com>
2022-01-19 14:32:09 -0600

    Add default config function to __init__.py

--- histomics_detect/models/__init__.py
+++ histomics_detect/models/__init__.py
@@ -3,8 +3,8 @@ This package contains Keras Model subclasses for detection
 """
 
 # make functions available at the package level using shadow imports
-from .faster_rcnn import FasterRCNN
-from .lnms_loss import normal_loss, clustering_loss, paper_loss
+from .faster_rcnn import FasterRCNN, faster_rcnn_config
+from .lnms_loss import normal_loss, xor_loss, clustering_loss, paper_loss
 from .compression_network import CompressionNetwork
 from .lnms_model import LearningNMS
 from .block_model import BlockModel
@@ -14,7 +14,9 @@ __all__ = (
 
     # functions and classes of this package
     'FasterRCNN',
+    'faster_rcnn_config',
     'normal_loss',
+    'xor_loss',
     'clustering_loss',
     'paper_loss',
     'CompressionNetwork',
