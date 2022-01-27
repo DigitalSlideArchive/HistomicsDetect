36ad5a70b6cf49fa7fb38257eee81284bc069247
Lee Cooper <cooperle@gmail.com>
2022-01-26 21:27:18 -0600

    Add rate metrics to __init__.py in metrics module

--- histomics_detect/metrics/__init__.py
+++ histomics_detect/metrics/__init__.py
@@ -3,17 +3,17 @@ This package contains performance metrics implemented in tensorflow
 """
 
 # make functions available at the package level using shadow imports
+from .average_precision import AveragePrecision
 from .iou import iou
 from .iou import greedy_iou_mapping
-from .average_precision import AveragePrecision
 from .lnms import tf_linear_sum_assignment
+from .objectness import FalseNegativeRate, FalsePositiveRate
 
-# list out things that are available for public use
 __all__ = (
-
-    # functions and classes of this package
+    'AveragePrecision',
     'iou',
     'greedy_iou_mapping',
-    'AveragePrecision',
-    'tf_linear_sum_assignment'
+    'tf_linear_sum_assignment',
+    'FalseNegativeRate',
+    'FalsePositiveRate'
 )
